2dd55d3246a6003372c7587c3a26387926cddae6
wxpsjhhd <62453755+wxpsjhhd@users.noreply.github.com>
2021-09-11 12:35:33 +0800

    Update test.py

--- 阿里云盘吧爬虫/test.py
+++ 阿里云盘吧爬虫/test.py
@@ -8,7 +8,7 @@ header = {
     'Connection': 'keep-alive',
     'User-Agent':' Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36'
 }
-key = input('请输入关键词:')
+key = 'idefun'
 url = f'https://www.google.com/search?q={key}'
 html = requests.get(url,header).content.decode('utf-8')
 with open('../index.html','w') as f:
