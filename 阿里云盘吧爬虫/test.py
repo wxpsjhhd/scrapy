411ab36200d9dd87d640a6a37fcf6b2f67856dcd
wxpsjhhd <wxp.0103@qq.com>
2021-09-11 12:34:02 +0800

    update

--- 阿里云盘吧爬虫/test.py
+++ 阿里云盘吧爬虫/test.py
@@ -1,5 +1,16 @@
 import requests
 
-url='https://www.google.com/search?q=中国'
-html=requests.get(url).content
+header = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'zh-CN,zh;q=0.9',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'keep-alive',
+    'User-Agent':' Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36'
+}
+key = input('请输入关键词:')
+url = f'https://www.google.com/search?q={key}'
+html = requests.get(url,header).content.decode('utf-8')
+with open('../index.html','w') as f:
+    f.write(html)
 print(html)
